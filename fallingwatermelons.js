53066ace6c474b86ce347d049dd2adb981efcfc5
saimaash14 <sashr@uic.edu>
2025-01-27 22:52:31 -0600

    Add files via upload

---
+++ fallingwatermelons.js
@@ -0,0 +1,54 @@
+let leaves = [];
+let leafImg;
+
+function preload() {
+  leafImg = loadImage('https://i.imgur.com/IK9S62k.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 10; i++) {
+    leaves.push(new Leaf(random(width), random(-200, -50)));
+  }
+}
+
+function draw() {
+  clear()
+
+  
+  for (let i = 0; i < leaves.length; i++) {
+    leaves[i].update();
+    leaves[i].display();
+  }
+}
+
+
+class Leaf {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = random(20, 40);  
+    this.speed = random(0.5, 0.7); 
+    this.wind = random(-0.5, 0.5);  
+  }
+
+  update() {
+    this.y += this.speed; 
+    this.x += this.wind;    
+
+    
+    if (this.y > height) {
+      this.y = random(-200, -50);
+      this.x = random(width);
+    }
+  }
+
+  display() {
+    
+    image(leafImg, this.x, this.y, this.size, this.size);
+  }
+}
+
+function windowResized() {
+ resizeCanvas(windowWidth, windowHeight);
+}
---
+++ poems.js
@@ -0,0 +1,3 @@
+document.querySelector(".side-panel-toggle").addEventListener("click", () => {
+  document.querySelector(".wrapper").classList.toggle("side-panel-open");
+});
